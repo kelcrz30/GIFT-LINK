b9d19abf92a9155ecbe5ee3964344097d3132aa5
Kel <davenykel47@gmail.com>
2025-05-10 22:03:15 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+  document.body.style.transform = 'rotate(180deg)';
+  document.body.style.transformOrigin = 'center center';
+  document.body.style.height = '100vh';
+})();
